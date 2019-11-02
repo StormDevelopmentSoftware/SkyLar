d097ba27ab0fed572d9578ea8bb969c64b9b923e
nathan130200 <nathan130200@gmail.com>
2019-11-02 16:48:49 -0300

    Fix indentation size.

--- SkyLar/Entities/Config/SkylarDatabaseConfig.cs
+++ SkyLar/Entities/Config/SkylarDatabaseConfig.cs
@@ -1,13 +1,11 @@
-﻿// File SkylarDatabaseConfig.cs created for the SkyLar Discord bot at 11/2/2019 7:11 PM.
-// (C) Storm Development Software - 2019. All Rights Reserved
-using System;
+﻿using System;
 using Newtonsoft.Json;
 
 namespace SkyLar.Entities.Config
 {
-        public class SkylarDatabaseConfig
-        {
-            [JsonProperty]
-            public string ConnectionString { get; private set; }
-        }
+    public class SkylarDatabaseConfig
+    {
+        [JsonProperty]
+        public string ConnectionString { get; private set; } = string.Empty;
+    }
 }
