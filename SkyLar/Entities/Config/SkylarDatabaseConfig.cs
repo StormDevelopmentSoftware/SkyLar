50ca4e7fce6d5f402ed98713ca8c5da7fa04f765
nathan130200 <nathan130200@gmail.com>
2019-11-02 16:49:39 -0300

    Replace default database uri.

--- SkyLar/Entities/Config/SkylarDatabaseConfig.cs
+++ SkyLar/Entities/Config/SkylarDatabaseConfig.cs
@@ -6,6 +6,6 @@ namespace SkyLar.Entities.Config
     public class SkylarDatabaseConfig
     {
         [JsonProperty]
-        public string ConnectionString { get; private set; } = string.Empty;
+        public string ConnectionString { get; private set; } = "mongodb://127.0.0.1";
     }
 }
