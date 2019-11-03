586dc9492ed79112863bf9301463a8493bada7b1
nathan130200 <nathan130200@gmail.com>
2019-11-03 13:42:24 -0300

    TODO: recuperar o uso de memoria por plataforma.

--- SkyLar/Commands/DevStatusCommand.cs
+++ SkyLar/Commands/DevStatusCommand.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
@@ -17,14 +19,42 @@ namespace SkyLar.Commands
         public async Task DevStatusCommand(CommandContext ctx)
         {
             var os = Environment.OSVersion.VersionString;
-            var memory = Process.GetCurrentProcess().PrivateMemorySize64;
 
             await ctx.RespondWithEmbedAsync(ctx.GetBaseEmbed()
                 .AddField("OS", os, true)
-                .AddField("RAM", memory.Bytes().Humanize("#.## MB"), true)
+                .AddField("RAM", GetMemoryUsage().Bytes().Humanize("#.## MB"), true)
                 .AddField("Ping", ctx.Client.Ping + "ms", true)
                 .AddField("DSharpPlus", ctx.Client.VersionString, true)
                 .AddField("Guilds", ctx.Client.Guilds.Count.ToString(), true));
+        }
+
+        // TODO
+
+        public static long GetMemoryUsage()
+        {
+            var process = Process.GetCurrentProcess();
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                return process.WorkingSet64;
+            else //if(RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD))
+            {
+                /*var file = new FileInfo($"/proc/{process.Id}/stat"));
+                
+                if (!file.Exists)
+                    return 0;
+
+                using(var reader = file.OpenText())
+                {
+                    string line;
+
+                    while((line = reader.ReadLine()) != null)
+                    {
+
+                    }
+                }*/
+
+                return 0;
+            }
         }
     }
 }
