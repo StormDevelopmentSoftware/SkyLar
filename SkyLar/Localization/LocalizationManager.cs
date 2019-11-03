be9264d632ad4fc206f9d29993819fb610db59bf
nathan130200 <nathan130200@gmail.com>
2019-11-03 11:48:38 -0300

    Corrigido DirectoryNotFoundException na tentativa de carregar a localização

--- SkyLar/Localization/LocalizationManager.cs
+++ SkyLar/Localization/LocalizationManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
 
@@ -9,13 +10,25 @@ namespace SkyLar.Localization
         public static List<Localization> Localizations = new List<Localization>();
         public static void Load()
         {
-            var directory = Directory.GetCurrentDirectory() + "/localizations/";
-            var files = Directory.GetFiles(directory, "*.json");
+            var directory = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "Localizations"));
 
-            foreach (var file in files)
+            if (!directory.Exists)
+                directory.Create();
+
+            foreach (var file in directory.GetFiles("*.json", SearchOption.TopDirectoryOnly))
             {
-                var locale = JsonConvert.DeserializeObject<Localization>(File.ReadAllText(file));
-                Localizations.Add(locale);
+                using (var reader = file.OpenText())
+                {
+                    try
+                    {
+                        var locale = JsonConvert.DeserializeObject<Localization>(reader.ReadToEnd());
+                        Localizations.Add(locale);
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine("Cannot load localization file {0}\n{1}", file.FullName, ex);
+                    }
+                }
             }
         }
     }
